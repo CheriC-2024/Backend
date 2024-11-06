102617e3f363f927b3588e9a6bca6b04f5365134
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-06 23:51:07 +0900

    :sparkles: feat: artist 엔티티 정의 추가 (#7)

---
+++ src/main/java/com/art/cheric/module/artist/domain/entity/Artist.java
@@ -0,0 +1,62 @@
+package com.art.cheric.module.artist.domain.entity;
+
+import com.art.cheric.global.common.BaseTime;
+import com.art.cheric.global.enums.ValidateState;
+import com.art.cheric.module.user.domain.entity.User;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+public class Artist extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @NotNull
+    private ValidateState state;
+
+    @Embedded
+    ArtistBank artistBank; // 은행 정보를 위한 VO
+
+    @Embedded
+    ArtistContact artistContact; // 소셜 정보를 위한 VO
+
+    public static Artist of(@NotNull User user) {
+        return Artist.builder()
+                .user(user)
+                .state(ValidateState.VALID_YET)
+                .build();
+    }
+
+    public void updateArtistBank(ArtistBank artistBank) {
+        this.artistBank = artistBank;
+    }
+
+    public void updateArtistContact(ArtistContact artistContact) {
+        this.artistContact = artistContact;
+    }
+
+    public void updateValidateState(ValidateState state) {
+        this.state = state;
+    }
+}
