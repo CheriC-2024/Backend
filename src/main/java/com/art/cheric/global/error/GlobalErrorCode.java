34760268b4d1dd87943a3dcd6b6ad7a55f18b120
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-10 18:09:07 +0900

    :sparkles: feat: 인증,인가 관련 에러 코드 추가 (#3)

--- src/main/java/com/art/cheric/global/error/GlobalErrorCode.java
+++ src/main/java/com/art/cheric/global/error/GlobalErrorCode.java
@@ -5,6 +5,14 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum GlobalErrorCode implements ErrorCode {
+    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "인증에 실패하였습니다."),
+    AUTHORIZATION_FAILED(HttpStatus.UNAUTHORIZED, "인가에 실패하였습니다."),
+    ACCESS_TOKEN_REQUIRED(HttpStatus.UNAUTHORIZED, "Access Token이 필요합니다."),
+    REFRESH_TOKEN_REQUIRED(HttpStatus.UNAUTHORIZED, "Refresh Token이 필요합니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token이 유효하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 사용자를 찾을 수 없습니다."),
+    LOGIN_REQUIRED(HttpStatus.FORBIDDEN, "로그인을 해주세요."),
+    EXPIRED_JWT(HttpStatus.FORBIDDEN, "Token이 만료되었습니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "요청 경로가 지원되지 않습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류가 발생했습니다.");
 
