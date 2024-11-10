fb93f01ee8b10f6f8095504ddf97098529f6ccf0
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-10 12:05:12 +0900

    :sparkles: feat: redis 값 처리 관련 메서드 util 추가 (#3)

---
+++ src/main/java/com/art/cheric/global/util/RedisUtil.java
@@ -0,0 +1,37 @@
+package com.art.cheric.global.util;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class RedisUtil {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    // 레디스에 유효 시간에 맞춰 값 저장하는 메서드
+    public void setOpsForValue(String key, String value, int expire_h) {
+        ValueOperations<String, Object> stringValueOperations = redisTemplate.opsForValue();
+        stringValueOperations.set(key, value);
+        redisTemplate.expire(key, expire_h, TimeUnit.HOURS);
+
+        log.info("[Redis] Data saved successfully. -- " + key);
+    }
+
+    // 레디스에 저장된 값 가져오는 메서드
+    public String getOpsForValue(String key) {
+        return (String) redisTemplate.opsForValue().get(key);
+    }
+
+    // 레디스에 저장된 값 삭제하는 메서드
+    public void delete(String key) {
+        redisTemplate.delete(key);
+
+        log.info("[Redis] Data deleted successfully. -- " + key);
+    }
+}
