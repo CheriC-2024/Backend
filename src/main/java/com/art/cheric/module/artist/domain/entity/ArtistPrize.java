fecc1984bc72fa79277a4b9e4c9f48980632d93d
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-23 17:44:38 +0900

    :bug: fix: 초기화되지 않고 있던 receivedAt 필드 초기화 진행 (#18)

--- src/main/java/com/art/cheric/module/artist/domain/entity/ArtistPrize.java
+++ src/main/java/com/art/cheric/module/artist/domain/entity/ArtistPrize.java
@@ -45,7 +45,7 @@ public class ArtistPrize extends BaseTime {
                 .artist(artist)
                 .organization(organization)
                 .level(level)
-                .level(level)
+                .receivedAt(receivedAt)
                 .build();
     }
 }
