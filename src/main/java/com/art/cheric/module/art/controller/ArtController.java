e64b76472d63ae0f04e33b79fa84ac47be6df6ca
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-23 17:33:14 +0900

    :recycle: refactor: 생성된 작품 id 반환 기능 추가 (#35)

--- src/main/java/com/art/cheric/module/art/controller/ArtController.java
+++ src/main/java/com/art/cheric/module/art/controller/ArtController.java
@@ -30,14 +30,14 @@ public class ArtController implements ArtControllerDocs {
     public ResponseEntity<ResponseDto> postOwnArt(@RequestAttribute("user") User user,
                                                   @RequestBody @Valid OwnArtReqDto ownArtReq) {
         Long artId = artService.postOwnArt(user, ownArtReq);
-        return ResponseEntity.status(201).body(ResponseDto.of(201, artId+" 작품이 생성되었습니다."));
+        return ResponseEntity.status(201).body(ResponseDto.of(201, artId + " 작품이 생성되었습니다."));
     }
 
     @PostMapping("/artist")
     public ResponseEntity<ResponseDto> postArtistArt(@RequestAttribute("user") User user,
                                                      @RequestBody @Valid ArtReqDto artReq) {
         Long artId = artService.postArtistArt(user, artReq);
-        return ResponseEntity.status(201).body(ResponseDto.of(201, artId+" 작품이 생성되었습니다."));
+        return ResponseEntity.status(201).body(ResponseDto.of(201, artId + " 작품이 생성되었습니다."));
     }
 
     @GetMapping("/{id}")
