a043e71dbd9ee260b8ddfb4038a4937194877b9e
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-23 17:46:08 +0900

    :bug: fix: 실제 작가 인증 여부 받은 사용자에 대한 이름 중복 검사 JPA 메소드로 수정 (#18)

--- src/main/java/com/art/cheric/module/user/domain/repository/UserRepository.java
+++ src/main/java/com/art/cheric/module/user/domain/repository/UserRepository.java
@@ -10,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 	Optional<User> findByEmail(String email);
-	Optional<User> findByIsArtistFalseAndName(String name);
+	Optional<User> findByIsValidateArtistFalseAndName(String name);
 }
