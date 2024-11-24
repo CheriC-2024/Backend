24dc31c1c8978bc6ebddf6c503646e929d6d0202
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-25 07:39:55 +0900

    :sparkles: feat: 작품 하트 시, 이미 작품 하트한 이력 확인 JPA 추가 (#41)

--- src/main/java/com/art/cheric/module/art/domain/repository/ArtHeartRepository.java
+++ src/main/java/com/art/cheric/module/art/domain/repository/ArtHeartRepository.java
@@ -1,10 +1,12 @@
 package com.art.cheric.module.art.domain.repository;
 
 import com.art.cheric.module.art.domain.entity.ArtHeart;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ArtHeartRepository extends JpaRepository<ArtHeart, Long> {
+    Optional<ArtHeart> findByArtIdAndUserId(Long artId, Long userId);
 }
