191044ed09b34124b2e82c997863a635ec7f4ce8
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-25 19:21:58 +0900

    :sparkles: feat: 전시 색상 리스트 저장 엔티티 추가 (#14)

---
+++ src/main/java/com/art/cheric/module/exhibition/domain/entity/ExhibitionBackgroundColor.java
@@ -0,0 +1,43 @@
+package com.art.cheric.module.exhibition.domain.entity;
+
+import com.art.cheric.global.common.BaseTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+public class ExhibitionBackgroundColor extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "exhibition_id", nullable = false)
+    private Exhibition exhibition;
+
+    @NotNull
+    private String colors;
+
+
+    public static ExhibitionBackgroundColor of(@NotNull Exhibition exhibition, @NotNull String colors) {
+        return ExhibitionBackgroundColor.builder()
+                .exhibition(exhibition)
+                .colors(colors)
+                .build();
+    }
+
+}
