f2773ea33757ccfe657fe6c8a57b5e674e13c57b
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-25 20:12:11 +0900

    :sparkles: feat: 전시 생성, 기본 정보 조회, 좋아요 생성, 좋아요 취소, 댓글 생성, 대댓글 생성, 댓글 좋아요, 댓글 좋아요 취소, 조회수 생성 관련 에러 코드 추가 (#14)

--- src/main/java/com/art/cheric/module/exhibition/error/ExhibitionErrorCode.java
+++ src/main/java/com/art/cheric/module/exhibition/error/ExhibitionErrorCode.java
@@ -8,7 +8,14 @@ import org.springframework.http.HttpStatus;
 public enum ExhibitionErrorCode implements ErrorCode {
     DUPLICATE_ART_ID(HttpStatus.BAD_REQUEST, "작품 ID는 한 요청에 한 개만 수용이 가능합니다."),
     INVALID_CLOUD_VISION_TYPE(HttpStatus.BAD_REQUEST, "지원하는 Cloud Vision Type이 아닙니다."),
-    INVALID_URL_PATH(HttpStatus.BAD_REQUEST, "요청한 작품의 이미지 경로가 유효하지 않습니다.");
+    INVALID_URL_PATH(HttpStatus.BAD_REQUEST, "요청한 작품의 이미지 경로가 유효하지 않습니다."),
+    EXHIBITION_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 전시가 존재하지 않습니다."),
+    EXHIBITION_HEART_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 해당 전시에 하트 표시를 하셨습니다."),
+    EXHIBITION_HEART_DOESNT_EXIST(HttpStatus.NOT_FOUND, "해당 전시에 하트 표시를 한 적이 없어 취소할 수 없습니다."),
+    REVIEW_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 전시 댓글이 존재하지 않습니다."),
+    EXHIBITION_REVIEW_HEART_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 해당 댓글에 하트 표시를 하셨습니다."),
+    EXHIBITION_REVIEW_HEART_DOESNT_EXIST(HttpStatus.NOT_FOUND, "해당 댓글에 하트 표시를 한 적이 없어 취소할 수 없습니다."),
+    EXHIBITION_BACKGROUND_INVALID(HttpStatus.BAD_REQUEST, "전시의 배경은 이미지 혹은 색상으로 설정되어야 합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
