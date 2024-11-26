f035db7a8afe097568b6b9a436904ab5391d9c8f
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-11-26 17:41:19 +0900

    :sparkles: feat: 컬렉션 커스텀 레포지토리 상속 관계 설정 (#17)

--- src/main/java/com/art/cheric/module/collection/domain/repository/CollectionRepository.java
+++ src/main/java/com/art/cheric/module/collection/domain/repository/CollectionRepository.java
@@ -1,12 +1,13 @@
 package com.art.cheric.module.collection.domain.repository;
 
 import com.art.cheric.module.collection.domain.entity.Collection;
+import com.art.cheric.module.collection.domain.repository.custom.CollectionRepositoryCustom;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CollectionRepository extends JpaRepository<Collection, Long> {
+public interface CollectionRepository extends JpaRepository<Collection, Long>, CollectionRepositoryCustom {
     Optional<Collection> findByUserIdAndName(Long userId, String name);
     Optional<Collection> findByIdAndUserId(Long collectionId, Long userID);
 }
