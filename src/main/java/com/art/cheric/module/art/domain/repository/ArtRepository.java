13e5184a6711c25829ffcc881d2823cb8217e2bd
yerim1ee <yearim1226@naver.com>
2024-11-27 07:08:17 +0900

    :sparkles: feat: 전시 커스텀 레포지토리 설정 (#11)

--- src/main/java/com/art/cheric/module/art/domain/repository/ArtRepository.java
+++ src/main/java/com/art/cheric/module/art/domain/repository/ArtRepository.java
@@ -1,10 +1,11 @@
 package com.art.cheric.module.art.domain.repository;
 
 import com.art.cheric.module.art.domain.entity.Art;
+import com.art.cheric.module.art.domain.repository.custom.ArtRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ArtRepository extends JpaRepository<Art, Long> {
+public interface ArtRepository extends JpaRepository<Art, Long>, ArtRepositoryCustom {
 }
---
+++ src/main/java/com/art/cheric/module/art/domain/repository/custom/ArtRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.art.cheric.module.art.domain.repository.custom;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArtRepositoryCustom {
+}
