5c754716721ea08eaabfc3bc65a02326c1310ab5
yerim1ee <yearim1226@naver.com>
2024-11-27 05:57:44 +0900

    :sparkles: feat: 전시 커스텀 레포지토리 설정 추가 (#17)

--- src/main/java/com/art/cheric/module/exhibition/domain/repository/ExhibitionRepository.java
+++ src/main/java/com/art/cheric/module/exhibition/domain/repository/ExhibitionRepository.java
@@ -1,9 +1,10 @@
 package com.art.cheric.module.exhibition.domain.repository;
 
 import com.art.cheric.module.exhibition.domain.entity.Exhibition;
+import com.art.cheric.module.exhibition.domain.repository.custom.ExhibitionRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
+public interface ExhibitionRepository extends JpaRepository<Exhibition, Long>, ExhibitionRepositoryCustom {
 }
