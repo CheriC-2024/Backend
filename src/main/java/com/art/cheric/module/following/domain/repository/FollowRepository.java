bfd05dd2b761ccbfde6a0b2eb703d8c5c5d58e0c
yerim <97941141+yerim123456@users.noreply.github.com>
2024-11-28 04:32:57 +0900

    :recycle: refactor: find JPA 메소드와 querydsl 충돌 가능성 기반 query dsl로 수정(#58)

--- src/main/java/com/art/cheric/module/following/domain/repository/FollowRepository.java
+++ src/main/java/com/art/cheric/module/following/domain/repository/FollowRepository.java
@@ -1,15 +1,11 @@
 package com.art.cheric.module.following.domain.repository;
 
 import com.art.cheric.module.following.domain.entity.Follow;
-
-import java.util.List;
-import java.util.Optional;
+import com.art.cheric.module.following.domain.repository.custom.FollowRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface FollowRepository extends JpaRepository<Follow, Long> {
-    Optional<Follow> findByFollowingUserIdAndFollowedUserId(Long followingUserId, Long followedUserId);
-    List<Follow> findByFollowingUserId(Long followingUserId);
+public interface FollowRepository extends JpaRepository<Follow, Long>, FollowRepositoryCustom {
 }
