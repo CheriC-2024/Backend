b6124984e70ab7186e11669beb9293190d57c862
yerim <97941141+yerim123456@users.noreply.github.com>
2024-11-28 04:25:52 +0900

    :sparkles: feat: 사용자 커스템 레포지토리 상속 설정 (#58)

--- src/main/java/com/art/cheric/module/user/domain/repository/UserRepository.java
+++ src/main/java/com/art/cheric/module/user/domain/repository/UserRepository.java
@@ -3,12 +3,12 @@ package com.art.cheric.module.user.domain.repository;
 import com.art.cheric.module.user.domain.entity.User;
 import java.util.Optional;
 
+import com.art.cheric.module.user.domain.repository.custom.UserRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String>, UserRepositoryCustom {
 	Optional<User> findByEmail(String email);
 	Optional<User> findByIsValidateArtistFalseAndName(String name);
 }
