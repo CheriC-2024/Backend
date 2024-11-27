f9ccac0b3b3ce7d9549128d8b99dd88e03d03459
yerim <97941141+yerim123456@users.noreply.github.com>
2024-11-27 21:53:15 +0900

    :sparkles: feat: 사용자 분야 가져오는 JPA 메서드 추가 (#11)

--- src/main/java/com/art/cheric/module/user/domain/repository/UserPartRepository.java
+++ src/main/java/com/art/cheric/module/user/domain/repository/UserPartRepository.java
@@ -4,7 +4,10 @@ import com.art.cheric.module.user.domain.entity.UserPart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface UserPartRepository extends JpaRepository<UserPart, Long> {
+    List<UserPart> findByUserId(Long userId);
 }
