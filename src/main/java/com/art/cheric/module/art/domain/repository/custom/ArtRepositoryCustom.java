d69c458322dfef645afc8a24e0d719ed0920c5e6
yerim <97941141+yerim123456@users.noreply.github.com>
2024-11-27 17:21:25 +0900

    :sparkles: feat: 작품 정렬 및 필터, 페이징에 따른 조회 메소드 추가 (#11)

--- src/main/java/com/art/cheric/module/art/domain/repository/custom/ArtRepositoryCustom.java
+++ src/main/java/com/art/cheric/module/art/domain/repository/custom/ArtRepositoryCustom.java
@@ -1,7 +1,15 @@
 package com.art.cheric.module.art.domain.repository.custom;
 
+import com.art.cheric.global.enums.ArtOrderType;
+import com.art.cheric.global.enums.ArtType;
+import com.art.cheric.module.art.domain.entity.Art;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtRepositoryCustom {
+    Page<Art> getArtsBySortAndFilterAndPaging(
+            Long userId, Boolean isCollectorsArt, ArtType artType, ArtOrderType order, Pageable pageable);
+
 }
