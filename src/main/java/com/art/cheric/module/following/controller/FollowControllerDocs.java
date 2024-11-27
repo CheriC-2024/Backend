2c170c6bf2b44bdd470a4a546f7b412b5dd15675
yerim <97941141+yerim123456@users.noreply.github.com>
2024-11-28 04:38:01 +0900

    :memo: docs: 팔로우 리스트 조회 API 문서화 (#58)

--- src/main/java/com/art/cheric/module/following/controller/FollowControllerDocs.java
+++ src/main/java/com/art/cheric/module/following/controller/FollowControllerDocs.java
@@ -1,6 +1,8 @@
 package com.art.cheric.module.following.controller;
 
 import com.art.cheric.global.common.ResponseDto;
+import com.art.cheric.global.enums.FollowSortType;
+import com.art.cheric.global.enums.UserOrderType;
 import com.art.cheric.module.user.domain.entity.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -53,7 +55,7 @@ public interface FollowControllerDocs {
     })
     ResponseEntity<ResponseDto> postFollow(User user, Long followedUserId);
 
-    @Operation(summary = "팔로잉 추가 API", description = "팔로잉 합니다.")
+    @Operation(summary = "팔로잉 삭제 API", description = "팔로잉 취소합니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "Ok",
                     content = @Content(
@@ -82,5 +84,56 @@ public interface FollowControllerDocs {
             )
     })
     ResponseEntity<ResponseDto> deleteFollow(User user, Long followedUserId);
+
+    @Operation(summary = "팔로잉/팔로워 리스트 조회 API", description = "팔로잉/팔로우 리스트를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Ok",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class),
+                            examples =
+                            @ExampleObject(value = "{\n" +
+                                    "  \"code\": 200,\n" +
+                                    "  \"message\": \"OK\",\n" +
+                                    "  \"data\": [\n" +
+                                    "    {\n" +
+                                    "      \"id\": 2,\n" +
+                                    "      \"name\": \"test1\",\n" +
+                                    "      \"profileImgUrl\": \"https://cheric-bucket.s3.ap-northeast-2.amazonaws.com/USER_IMG/1/716dc032-40da-4e9a-97a1-e27ea8abbbd2-profileimage.png\",\n" +
+                                    "      \"artTypes\": [\n" +
+                                    "        \"ORIENTAL_PAINTING\",\n" +
+                                    "        \"DESIGN_ART\"\n" +
+                                    "      ],\n" +
+                                    "      \"following\": true\n" +
+                                    "    },\n" +
+                                    "    {\n" +
+                                    "      \"id\": 3,\n" +
+                                    "      \"name\": \"test2\",\n" +
+                                    "      \"profileImgUrl\": \"https://cheric-bucket.s3.ap-northeast-2.amazonaws.com/USER_IMG/1/716dc032-40da-4e9a-97a1-e27ea8abbbd2-profileimage.png\",\n" +
+                                    "      \"artTypes\": [\n" +
+                                    "        \"WATER_PAINTING\",\n" +
+                                    "        \"OIL_PAINTING\"\n" +
+                                    "      ],\n" +
+                                    "      \"following\": true\n" +
+                                    "    }\n" +
+                                    "  ],\n" +
+                                    "  \"totalElements\": 2,\n" +
+                                    "  \"totalPages\": 1,\n" +
+                                    "  \"size\": 10,\n" +
+                                    "  \"numberOfElements\": 2\n" +
+                                    "}")
+                    )
+            ),
+            @ApiResponse(responseCode = "404", description = "해당 자원을 찾을 수 없습니다.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class),
+                            examples =
+                            @ExampleObject(value = "{ \"code\": 404, \"message\": \"해당하는 사용자를 찾을 수 없습니다.\" }"
+                            )
+                    )
+            )
+    })
+    ResponseEntity<ResponseDto> getFollowList(Long userId, FollowSortType sort, UserOrderType order, int page, int size);
 }
 
