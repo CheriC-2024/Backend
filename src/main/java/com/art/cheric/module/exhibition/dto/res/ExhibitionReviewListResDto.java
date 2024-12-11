74c5beac06e4f049049520adf3bf0ac460ae001d
yerim1ee <yearim1226@naver.com>
2024-12-12 08:57:17 +0900

    :sparkles: feat: 전시 댓글 리스트 작성자 이미지 및 좋아요 여부 반환 값 추가 (#67)

--- src/main/java/com/art/cheric/module/exhibition/dto/res/ExhibitionReviewListResDto.java
+++ src/main/java/com/art/cheric/module/exhibition/dto/res/ExhibitionReviewListResDto.java
@@ -19,6 +19,9 @@ public class ExhibitionReviewListResDto {
     @Schema(description = "댓글 작성자 이름", example = "체리시")
     private final String name;
 
+    @Schema(description = "댓글 작성자 이미지", example = "https://cheric-bucket.s3.ap-northeast-2.amazonaws.com/USER_IMG/1/716dc032-40da-4e9a-97a1-e27ea8abbbd2-profileimage.png")
+    private final String imgUrl;
+
     @Schema(description = "좋아요 수", example = "12")
     private final int heartCount;
 
@@ -28,16 +31,20 @@ public class ExhibitionReviewListResDto {
     @Schema(description = "댓글 작성 시간", example = "2024-12-23")
     private final String createAt;
 
+    @Schema(description = "좋아요 여부", example = "true")
+    private final Boolean isHeart;
 
-    public static ExhibitionReviewListResDto of(Long id, String review, String name, int heartCount, int replyCount,
-                                                String createAt) {
+    public static ExhibitionReviewListResDto of(Long id, String review, String name, String imgUrl, int heartCount,
+                                                int replyCount, String createAt, Boolean isHeart) {
         return ExhibitionReviewListResDto.builder()
                 .id(id)
                 .review(review)
                 .name(name)
+                .imgUrl(imgUrl)
                 .heartCount(heartCount)
                 .replyCount(replyCount)
                 .createAt(createAt)
+                .isHeart(isHeart)
                 .build();
     }
 
