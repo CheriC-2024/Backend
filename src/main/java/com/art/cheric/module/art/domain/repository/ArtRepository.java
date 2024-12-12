6acd8db983205e78158f49ed638b1901db6ee168
yerim1ee <yearim1226@naver.com>
2024-12-12 23:02:05 +0900

    :sparkles: feat: 가장 최근의 작품 1개 가져오기 JPA 메서드 추가 (#69)

--- src/main/java/com/art/cheric/module/art/domain/repository/ArtRepository.java
+++ src/main/java/com/art/cheric/module/art/domain/repository/ArtRepository.java
@@ -2,7 +2,11 @@ package com.art.cheric.module.art.domain.repository;
 
 import com.art.cheric.module.art.domain.entity.Art;
 import com.art.cheric.module.art.domain.repository.custom.ArtRepositoryCustom;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +15,8 @@ import java.util.Optional;
 @Repository
 public interface ArtRepository extends JpaRepository<Art, Long>, ArtRepositoryCustom {
     Optional<Art> findByIdAndUserId(Long id, Long userId);
+
+    @Query("SELECT a FROM Art a WHERE a.user.id = :userId ORDER BY a.createdAt DESC")
+    List<Art> findMostRecentArtByUserId(@Param("userId") Long userId, Pageable pageable);
+
 }
